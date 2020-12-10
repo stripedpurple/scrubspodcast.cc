651f6b0c31035aaf0450fc27ed4bc96ea7f5cd04
Austin Barrett <virus.cmd@gmail.com>
2020-12-09 22:28:52 -0500

    about page

--- pages/about.vue
+++ pages/about.vue
@@ -1,13 +1,39 @@
 <template>
   <section class="section">
     <div class="container">
+      <h1 class="title">
+        About...
+      </h1>
 
+      <h2 class="subtitle">Fake Doctors Real Friends</h2>
+      <p>You know what's long, tedious and boring? Surgery. You know what isn't? This new podcast! Join Scrubs co-stars
+        and real-life best
+        friends Zach Braff and Donald Faison for a weekly comedy podcast where they relive the hit TV show, one episode
+        at a time. Each week,
+        these BFFs will discuss an episode of Scrubs, sharing behind-the-scenes stories and reminiscing on some of their
+        favorite memories from
+        filming. They’ll also connect with Scrubs super fans and feature beloved show cast members for exclusive
+        interviewsx</p>
+
+      <h2 class="subtitle">This site</h2>
+      <p>Hi my name is Austin Barrett (<a href="https://instagram.com/stripedpurple.io">@stripedpurple</a>) and I am a
+        longtime fan of Scrubs. Recently I have been on a podcast binge. This
+        site is not officially affiliated with the podcast or Iheart Radio, and is completely fan made and maintain.</p>
+      <p>I made this site purely out of love for Scrubs, Zach Braff, Donald Faison, Joelle Monique, Danl Goodman, and
+        the podcast, and i don't make any money from its existence. Thanks to all of you for bringing a little laughter
+        into these all too
+        draconian days</p>
+
+      <p>If you have any questions, or want to site anything added to the site feel free to reach out toi me or make a
+        pull request</p>
+      <p><a href="https://github.com/viruscmd/scrubspodcast.cc">https://github.com/viruscmd/scrubspodcast.cc</a></p>
     </div>
+
   </section>
 </template>
 
 <script>
-export default {
-  name: 'HomePage',
-}
+  export default {
+    name: 'About',
+  }
 </script>
